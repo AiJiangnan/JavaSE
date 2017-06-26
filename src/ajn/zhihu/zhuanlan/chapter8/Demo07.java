9c69aeb115d75663f6124a6fc8e2caa26d965e02
AiJiangnan <904629998@qq.com>
2017-06-26 21:15:10 +0800

    第8章完成

--- src/ajn/zhihu/zhuanlan/chapter8/Demo07.java
+++ src/ajn/zhihu/zhuanlan/chapter8/Demo07.java
@@ -6,24 +6,21 @@ public class Demo07 {
 	public static void main(String[] args) {
 		int[] a = {10,9,8,7,6,5,4,3,2,1};
 		Arrays.sort(a);
-		int elem = 5;
+		System.out.println(Arrays.toString(a));
+		int elem = 7;		//要查询的元素
 		int index = 0;
 		int iStart = 0;
-		int iEnd = a.length-1;
-		int searchCount = 0;
+		int iEnd = a.length;
 		for(int i=0;i<a.length/2;i++) {
-			searchCount++;
 			index = (iStart+iEnd)/2;
 			if(a[index]<elem){
-				System.out.println("aa");
 				iStart = index;
 			}else if(a[index]>elem){
-				System.out.println("bb");
 				iEnd = index;
 			}else{
+				System.out.println(index);
 				break;
 			}
 		}
-		System.out.println(searchCount);
 	}
 }
