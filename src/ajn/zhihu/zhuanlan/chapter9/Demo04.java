decfef96bbd7b7063fc62fdce8921aacbade6b34
AiJiangnan <904629998@qq.com>
2017-06-29 18:56:08 +0800

    第9章完成

---
+++ src/ajn/zhihu/zhuanlan/chapter9/Demo04.java
@@ -0,0 +1,9 @@
+package ajn.zhihu.zhuanlan.chapter9;
+
+public class Demo04 {
+	public static void main(String[] args) {
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+}
