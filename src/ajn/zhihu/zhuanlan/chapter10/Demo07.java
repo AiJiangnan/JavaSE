433df210d86cddf67257af014ffc65e7c50d27b7
AiJiangnan <904629998@qq.com>
2017-07-01 16:15:13 +0800

    第10章-封装

---
+++ src/ajn/zhihu/zhuanlan/chapter10/Demo07.java
@@ -0,0 +1,25 @@
+package ajn.zhihu.zhuanlan.chapter10;
+
+public class Demo07 {
+	public static void main(String[] args) {
+		new Static();
+	}
+	// print:
+	//	I am a static block!
+	//	I am a normal block!
+	//	I am a constructor!
+}
+
+class Static{
+	static{
+		System.out.println("I am a static block!");
+	}
+	
+	{
+		System.out.println("I am a normal block!");
+	}
+
+	public Static() {
+		System.out.println("I am a constructor!");
+	}
+}
