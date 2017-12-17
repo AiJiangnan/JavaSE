0912fa63fa85fe3c7173ebe0e84cfa470a1861a4
AiJiangnan <904629998@qq.com>
2017-12-17 13:37:49 +0800

    JavaSE页面分支

--- src/ajn/zhihu/zhuanlan/chapter4/Demo05.java
+++
@@ -1,9 +0,0 @@
-package ajn.zhihu.zhuanlan.chapter4;
-
-public class Demo05 {
-	public static void main(String args[]){
-		double a = 2.6;
-		a = a - 2;
-		System.out.println(a);		//print: 0.6000000000000001
-	}
-}
